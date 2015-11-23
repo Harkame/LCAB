1bbd652a71faa2c8c67f02bf17073be7833bc746
masskarad <masskaarad@gmail.com>
2015-11-23 21:46:06 +0100

    Update PlateauBulle.java

--- PlateauBulle.java
+++ PlateauBulle.java
@@ -7,57 +7,35 @@ import java.awt.Frame;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.Dimension;
-
-
-
+import java.util.ArrayList;
 
 public class PlateauBulle extends JFrame { // création de ma fenêtre 
 	public static JPanel pan;	
-
+	public ArrayList<Annimation> animations = new ArrayList<Annimation>();
 	
 	/**
 	 */
-	public PlateauBulle(){
+	public PlateauBulle(int nb){
 		this.setTitle("La case à bulles");
-		  this.setSize(1000, 1000);
+		this.setSize(1000, 1000);
 		this.setExtendedState(Frame.MAXIMIZED_BOTH); // fenetre plein écrant
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // stopper le programme en au click sur la croix
 		this.pan =new JPanel(); // instanciation de mon conteneur
 		this.setContentPane(pan); // je choisi mon contenneur 
 		this.setVisible(true); // rentdre ma fenêtre visible
 		this.pan.setLayout(null); // aucune disposition par défault dans mon conteneur ce qui permet de déplacer comme on veut notre label
-
+		for (int i = 0; i<nb; i++){
+			animations.add(new Annimation("anim", pan));
+		}
+		for (int i = 0; i<nb; i++){
+			animations.get(i).start();
+		}
 									
 	}
 	
-	
-	
-		
-	
 	public static void main(String[]args){
-	PlateauBulle plat=new PlateauBulle();
-		/*Bulle maBulle=new Bulle(5,9,151,151,plat.pan);
-		
-		Bulle maBulle2=new Bulle(500,500,151,151,plat.pan);
-		
-		maBulle.animer();*/
-		
-		
-		Annimation a= new Annimation("anim1",pan);
-		Annimation b= new Annimation("anim1",pan);
-		Annimation c= new Annimation("anim1",pan);
-		Annimation d= new Annimation("anim1",pan);
-		a.start();
-		b.start();
-		c.start();
-		d.start();
-		
-		
-		
-		
+	PlateauBulle plat=new PlateauBulle(1);
 		// après cette instruction rien ne peut s'excuter car j'utilise un true dans la condition d'un tant que 
-		
-		
-				
+
 	}
 }
