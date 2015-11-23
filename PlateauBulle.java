ab713cecba303d4703eddf61be88755d950694f2
dydy971 <m.lildy971@gmail.com>
2015-11-23 11:05:35 +0100

    Create PlateauBulle.java

---
+++ PlateauBulle.java
@@ -0,0 +1,48 @@
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.Frame;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.Dimension;
+
+
+
+
+public class PlateauBulle extends JFrame { // création de ma fenêtre 
+	public JPanel pan;	
+
+	
+	/**
+	 */
+	public PlateauBulle(){
+		this.setTitle("La case à bulles");
+		  this.setSize(1000, 1000);
+		this.setExtendedState(Frame.MAXIMIZED_BOTH); // fenetre plein écrant
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // stopper le programme en au click sur la croix
+		this.pan =new JPanel(); // instanciation de mon conteneur
+		this.setContentPane(pan); // je choisi mon contenneur 
+		this.setVisible(true); // rentdre ma fenêtre visible
+		this.pan.setLayout(null); // aucune disposition par défault dans mon conteneur ce qui permet de déplacer comme on veut notre label
+
+									
+	}
+	
+	
+	
+		
+	
+	public static void main(String[]args){
+	PlateauBulle plat=new PlateauBulle();
+		Bulle maBulle=new Bulle(5,9,151,151,plat.pan);
+		
+		Bulle maBulle2=new Bulle(500,500,151,151,plat.pan);
+		
+		maBulle.animer();// après cette instruction rien ne peut s'excuter car j'utilise un true dans la condition d'un tant que 
+		
+		
+				
+	}
+}
