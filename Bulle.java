65628e0896c6dae2b8a49a81615f1f99c1b463ee
masskarad <masskaarad@gmail.com>
2015-11-24 19:57:58 +0100

    Update Bulle.java

--- Bulle.java
+++ Bulle.java
@@ -1,12 +1,10 @@
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.Dimension;
-
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
 
 public class Bulle {
 	private JPanel pan;
@@ -15,34 +13,50 @@ public class Bulle {
 	private int haut;
 	private int x;
 	private int y;
-	private static Icon image = new ImageIcon("bulle.png");
-	private static int compteBulle = 0;
+	private static Icon image = new ImageIcon("bulle.png");	private static Icon image2 = new ImageIcon("bb.png");
+        private static int cpt=0;
 	
 							
 	public Bulle(int x, int y, int larg, int haut,JPanel pan){
-		
-		this.label =new JLabel(image); 
-		this.x=x;
+		int a = (int)(Math.random()+0.5);
+		if (a == 0){
+			this.label =new JLabel(image);
+		}
+		else {this.label =new JLabel(image2); 
+		}this.x=x;
 		this.y=y;
 		this.larg=larg;
 		this.haut=haut;
 		this.pan=pan;
-		  this.label.addMouseListener(new MouseAdapter(){  public void mousePressed(MouseEvent me){
-	          
-              	compteBulle++;
-          	  label.setVisible(false);
-                   //Do action here    	
-   }});	
-	  		 this.pan.add(this.label);
-	  		 this.label.setBounds(this.x,this.y,this.larg,this.haut);
+		this.label.addMouseListener(new MouseAdapter(){  public void mousePressed(MouseEvent me){
+	          cpt++;
+              		System.out.println(cpt);
+          	  	label.setVisible(false);
+                   	//Do action here    	
+   		}});	
+	  	this.pan.add(this.label);
+	  	this.label.setBounds(this.x,this.y,this.larg,this.haut);
 	}
 
-	/*public void afficherBulle(JPanel pan){
-		pan.add(this.label);
-		this.label.setBounds(this.x,this.y,this.larg,this.haut);
-	}*/
+	public char colision(){
+		char cote= 'a';
+		for (int i =0; i<PlateauBulle.anim.length; i++){
+			if (this.x!=PlateauBulle.anim[i].bulle.getX()&&this.y!=PlateauBulle.anim[i].bulle.getY()){
+				if(((this.x-PlateauBulle.anim[i].bulle.getX())*(this.x-PlateauBulle.anim[i].bulle.getX())+(this.y-PlateauBulle.anim[i].bulle.getY())*(this.y-PlateauBulle.anim[i].bulle.getY()))<=151*151){
+					
+					if((this.x-PlateauBulle.anim[i].bulle.getX())>(0-150)&&(this.y+75>PlateauBulle.anim[i].bulle.getY())&&(this.y-75<PlateauBulle.anim[i].bulle.getY())){cote= 'd';}
+					else if(((this.x-PlateauBulle.anim[i].bulle.getX())<150)&&(this.y+75>PlateauBulle.anim[i].bulle.getY())&&(this.y-75<PlateauBulle.anim[i].bulle.getY())){cote= 'g';}
+					else if((this.y-PlateauBulle.anim[i].bulle.getY())>(0-150)&&(this.x+75>PlateauBulle.anim[i].bulle.getX())&&(this.x-75<PlateauBulle.anim[i].bulle.getX())){cote= 'h';}
+					else if(((this.y-PlateauBulle.anim[i].bulle.getY())<150)&&(this.x+75>PlateauBulle.anim[i].bulle.getX())&&(this.x-75<PlateauBulle.anim[i].bulle.getX())){cote= 'b';}
+				}
+			}
+		}
+		
+		return cote;
+	}
+
 	public void animer(){
-	
+
 		
 		Dimension dimension = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
 		int height = (int)dimension.getHeight();
@@ -51,58 +65,56 @@ public class Bulle {
 		this.y=(int)(Math.random()*(height-215));
 		int signal=0;
 		int signaly=0;
+		char coli;
 		while(true){
-			
-				this.label.setBounds(this.x,this.y,151,151);
-				if(this.x==(width-150)){
-					signal=1;	
-				}
-				if(this.y==(height-215)){
-					signaly=1;
-				}
-				if(signal==1&&signaly==1){
-					this.x=this.x-1;
-					this.y=this.y-1;
-					if(this.x==0){
-						signal=0;
-					}
-					if(this.y==0){
-						signaly=0;
-					}
-				}
-				else if(signal==1&&signaly==0){
-					this.x=this.x-1;
-					this.y=this.y+1;
-					if(this.x==0){
-						signal=0;
-					}
-					
-				}
-				else if(signal==0&&signaly==1){
-					this.x=this.x+1;
-					this.y=this.y-1;
-					if(this.y==0){
-						signaly=0;
-					}
-				}
-				else if(signal==0&&signaly==0){
-					this.x=this.x+1;
-					this.y=this.y+1;
-				}
-				try {
+		
+			this.label.setBounds(this.x,this.y,151,151);
+			coli = this.colision();
+			if(this.x==(width-150)||coli=='d'){
+				signal=1;	
+			}
+			else if(this.x==0||coli=='g'){
+				signal=0;
+			}
 
-					Thread.sleep(3);
+			if(this.y==(height-215)||coli=='h'){
+				signaly=1;
+			}
+			else if(this.y==0||coli=='b'){
+				signaly=0;
+			}
+			if(signal==1&&signaly==1){
+				this.x=this.x-1;
+				this.y=this.y-1;
+			}
+			else if(signal==1&&signaly==0){
+				this.x=this.x-1;
+				this.y=this.y+1;
+			}
+			else if(signal==0&&signaly==1){
+				this.x=this.x+1;
+				this.y=this.y-1;
+			}
+			else if(signal==0&&signaly==0){
+				this.x=this.x+1;
+				this.y=this.y+1;
+			}
+			try {
 
-				} catch (InterruptedException e) {
+				Thread.sleep(3);
 
-					e.printStackTrace();
+			} catch (InterruptedException e) {
 
-				}
-				}	// fin tant que
-		}
+				e.printStackTrace();
+			}
+		}	// fin tant que
+	}
 
+	public int getX(){
+		return this.x;
+	}
 
-		public static int getCompte(){
-			return this.compteBulle;
-		}
+	public int getY(){
+		return this.y;
+	}
 }
