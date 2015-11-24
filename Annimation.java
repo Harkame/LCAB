392897c2010929651302364b1bd947af8530baf0
masskarad <masskaarad@gmail.com>
2015-11-24 08:06:54 +0100

    Update Annimation.java

--- Annimation.java
+++ Annimation.java
@@ -1,18 +1,17 @@
 import javax.swing.JPanel;
 
 public class Annimation extends Thread {
-		private JPanel pan;
-		private Bulle bulle;
+	private JPanel pan;
+	private Bulle bulle;
 
 	public Annimation(String nom, JPanel pan) {
 		super(nom);
 		this.pan=pan;
 	}
-	 
-		
-	 public void run() {
+
+	public void run() {
 		this.bulle=new Bulle(500,500,151,151,this.pan);
 		this.bulle.animer();
 	   	Thread.yield();	  
-	 }	
+	}	
 }
