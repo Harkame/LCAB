bc0caf5ba5650f58f400f2e602c5f530e042246e
dydy971 <m.lildy971@gmail.com>
2015-11-30 18:24:02 +0100

    Update Bulle.java

--- Bulle.java
+++ Bulle.java
@@ -9,24 +9,63 @@ import javax.swing.JPanel;
 public class Bulle {
 	private JPanel pan;
 	private JLabel label;	
-	private int larg;
-	private int haut;
+	private int margx;
+	private int margy;
 	private int x;
 	private int y;
-	private static Icon image = new ImageIcon("bulle.png");	private static Icon image2 = new ImageIcon("bb.png");
-        private static int cpt=0;
+	private int taille; 
+	private long vitesseBul;
+	private static Icon image;	//private static Icon image2 = new ImageIcon("bb.png");
+    private static int cpt=0;
 	
 							
-	public Bulle(int x, int y, int larg, int haut,JPanel pan){
-		int a = (int)(Math.random()+0.5);
+	public Bulle(int x, int y, long vitesseBul,int taille,JPanel pan){
+		/*int a = (int)(Math.random()+0.5);
 		if (a == 0){
 			this.label =new JLabel(image);
 		}
 		else {this.label =new JLabel(image2); 
-		}this.x=x;
+		}*/
+			this.taille=taille;
+			switch(this.taille){
+			case 1:
+				this.margx=115;
+				this.margy=115;
+				image= new ImageIcon("bulle.png");
+			break;
+			case 2:
+				this.margx=151;
+				this.margy=151;
+				image= new ImageIcon("bulle2.png");
+			break;
+			case 3:
+				this.margx=255;
+				this.margy=255;
+				image= new ImageIcon("bulle3.png");
+			break;
+			case 4:
+				this.margx=350;
+				this.margy=353;
+				image= new ImageIcon("bulle4.png");
+			break;
+			case 5:
+				this.margx=520;
+				this.margy=523;
+				image= new ImageIcon("bulle5.png");
+			break;
+			 default:
+				this.margx=115;
+				this.margy=115;
+				image= new ImageIcon("bulle.png");
+			break;
+			
+		}
+			
+			
+		this.label =new JLabel(image);
+		this.vitesseBul=vitesseBul;
+		this.x=x;
 		this.y=y;
-		this.larg=larg;
-		this.haut=haut;
 		this.pan=pan;
 		this.label.addMouseListener(new MouseAdapter(){  public void mousePressed(MouseEvent me){
 	          cpt++;
@@ -35,10 +74,11 @@ public class Bulle {
                    	//Do action here    	
    		}});	
 	  	this.pan.add(this.label);
-	  	this.label.setBounds(this.x,this.y,this.larg,this.haut);
+	  	this.label.setBounds(this.x,this.y,this.margx,this.margy);
 	}
 
-	public char colision(){
+
+	/*public char colision(){
 		char cote= 'a';
 		for (int i =0; i<PlateauBulle.anim.length; i++){
 			if (this.x!=PlateauBulle.anim[i].bulle.getX()&&this.y!=PlateauBulle.anim[i].bulle.getY()){
@@ -68,7 +108,7 @@ public class Bulle {
 		char coli;
 		while(true){
 		
-			this.label.setBounds(this.x,this.y,151,151);
+			this.label.setBounds(this.x,this.y,this.margx,this.margy);
 			coli = this.colision();
 			if(this.x==(width-150)||coli=='d'){
 				signal=1;	
@@ -101,14 +141,14 @@ public class Bulle {
 			}
 			try {
 
-				Thread.sleep(3);
+				Thread.sleep(this.vitesseBul);
 
 			} catch (InterruptedException e) {
 
 				e.printStackTrace();
 			}
 		}	// fin tant que
-	}
+	}*/
 
 	public int getX(){
 		return this.x;
@@ -117,4 +157,57 @@ public class Bulle {
 	public int getY(){
 		return this.y;
 	}
+	public int getMargx(){
+		return this.margx;
+	}
+	public int getMargy(){
+		return this.margy;
+	}
+	public int getTailleBulle(){
+		return this.taille;
+	}
+	public void setTailleBulle(int newTaille){
+		this.taille=newTaille;
+		switch(this.taille){
+		case 1:
+			this.margx=115;
+			this.margy=115;
+			image= new ImageIcon("bulle.png");
+		break;
+		case 2:
+			this.margx=151;
+			this.margy=151;
+			image= new ImageIcon("bulle2.png");
+		break;
+		case 3:
+			this.margx=255;
+			this.margy=255;
+			image= new ImageIcon("bulle3.png");
+		break;
+		case 4:
+			this.margx=350;
+			this.margy=353;
+			image= new ImageIcon("bulle4.png");
+		break;
+		case 5:
+			this.margx=520;
+			this.margy=523;
+			image= new ImageIcon("bulle5.png");
+		break;
+		 default:
+			this.margx=115;
+			this.margy=115;
+			image= new ImageIcon("bulle.png");
+		break;
+		
+	}
+		this.label.setIcon(image);
+	}
+	public long getVitesseBulle(){
+		return this.vitesseBul;
+	}
+	public void setVitesseBulle(long newVitesseBulle){
+		this.vitesseBul=newVitesseBulle;
+	}
 }
+
