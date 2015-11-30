b8997e1447914c3914657a8a45b336cf33e16c5a
PraetCom <cedric_duriez@hotmail.fr>
2015-11-30 23:09:27 +0100

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,36 +1,158 @@
-package bulles;
+package projet_bulles;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
-
-import java.awt.Graphics;
+import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.Graphics2D;
-import java.awt.Rectangle;
+import java.awt.Frame;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import projet_bulles.Jeu.STATE;
 
 
 
-public class Menu {
-
-		public	Rectangle JouerBoutton = new Rectangle(Game.WIDTH / 2 + 120, 150, 300, 50);
-		public	Rectangle ChoixNiveauBoutton = new Rectangle(Game.WIDTH / 2 + 120, 250, 300, 50);
-		public	Rectangle QuitterBoutton = new Rectangle(Game.WIDTH / 2 + 120, 350, 300, 50);
+public class Menu extends JFrame {
+	
+	int SCALE = 2;
+	String title = "Jeu de Bulles";
+	
+	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	int width = (int) screenSize.getWidth();
+	int height = (int) screenSize.getHeight();
+	
+	private JButton BoutonJouer;
+	private JButton BoutonChoisirNiveau;
+	private JButton BoutonOptions;
+	private JButton BoutonQuitterJeu;
+	private JButton BoutonRevenirConnexion;
+	
+	
+	
+	
+	public Menu()  throws IOException{
+		
+		super("Jeu De Bulles");
+		setSize(width,height);
+		
+		/* Redimensionner les images pour travailler sur tous les écrans */
+		// if(width == 1920 && height == 1080) {
+		this.setContentPane(new ImagePanel(new ImageIcon("wallpaper.jpg").getImage()));
+		 //}
+		 
+			this.setLayout(null); // permet le position correcte des boutons
 			
-			
+		 Font police = new Font("Verdana", Font.BOLD, 20);
+			this.BoutonJouer = new JButton("Jouer");
+			this.BoutonJouer.setBackground(Color.WHITE);
+			this.BoutonJouer.setFont(police);
+			this.BoutonJouer.setBounds(width /2 -110, 400, 200, 75);
+		this.getContentPane().add(this.BoutonJouer, BorderLayout.CENTER);
+		
+		
+		
+		this.BoutonJouer.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Jeu.State = STATE.GAME;
+				dispose();
+				try {
+					Jeu.controller();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 				
-				Graphics g2d = (Graphics2D) g;
-			Font fnt0= new Font("arial", Font.BOLD, 50);
-			g.setFont(fnt0);
-			g.setColor(Color.white);
-			g.drawString("Jeu de bulles", Game.WIDTH / 2, 100); // Game variable à créer ou taille à définir
-			Font fnt1 = new Font("arial", Font.BOLD, 30);
-			g.setFont(fnt1);
-			g.drawString("Jouer", JouerBoutton.x + 19,JouerBoutton.y + 30);
-			((Graphics2D) g2d).draw(JouerBoutton);
-			g.drawString("Choix Niveau", ChoixNiveauBoutton.x + 19, ChoixNiveauBoutton.y + 30);
-			((Graphics2D) g2d).draw(ChoixNiveauBoutton);
-			g.drawString("Quitter", QuitterBoutton.x + 19, QuitterBoutton.y + 30);
-			((Graphics2D) g2d).draw(QuitterBoutton);
+			}
 			
+		});
+		
+	
+			this.BoutonChoisirNiveau = new JButton("Choisir Niveau");
+			this.BoutonChoisirNiveau.setBackground(Color.WHITE);
+			this.BoutonChoisirNiveau.setFont(police);
+			this.BoutonChoisirNiveau.setBounds(width /2 -110, 500, 200, 75);
+		this.getContentPane().add(this.BoutonChoisirNiveau, BorderLayout.CENTER);
+		
+
+		this.BoutonOptions = new JButton("Options");
+		this.BoutonOptions.setBackground(Color.WHITE);
+		this.BoutonOptions.setFont(police);
+		this.BoutonOptions.setBounds(width /2 -110, 600, 200, 75);
+	this.getContentPane().add(this.BoutonOptions, BorderLayout.CENTER);
+	
+	
+		
+
+		this.BoutonQuitterJeu = new JButton("Quitter le jeu");
+		this.BoutonQuitterJeu.setBackground(Color.WHITE);
+		this.BoutonQuitterJeu.setFont(police);
+		this.BoutonQuitterJeu.setBounds(width /2 -110, 700, 200, 75);
+	this.getContentPane().add(this.BoutonQuitterJeu, BorderLayout.CENTER);
+	
+	this.BoutonQuitterJeu.addActionListener(new ActionListener() {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			System.gc();
+			System.exit(0);
+			
+			
+		}
+		
+	});
+	
+	this.BoutonRevenirConnexion = new JButton("Revenir à l'écran de connexion");
+	this.BoutonRevenirConnexion.setBackground(Color.ORANGE);
+	this.BoutonRevenirConnexion.setFont(police);
+	this.BoutonRevenirConnexion.setBounds(100, 900, 500, 75);
+this.getContentPane().add(this.BoutonRevenirConnexion, BorderLayout.CENTER);
+
+this.BoutonRevenirConnexion.addActionListener(new ActionListener() {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		Jeu.State = STATE.IDENTIFICATIONS;
+		dispose();
+		try {
+			Jeu.controller();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
+	
+});
+	
+	
+		 
+		 
+		setResizable(false);
+	
+		
+		this.setVisible(true);
+
+//		
+	}
+	
+	
+	public static void main(String[] args) throws IOException {
+	
+		Menu m1 = new Menu();
+		//	new Menu2().setVisible(true);
+		
+		// id1.execute();
+	}
+	
 	
 }
