e760dc04634246553bf4c48c03d903cec8d09e53
PraetCom <cedric_duriez@hotmail.fr>
2015-11-30 23:07:35 +0100

    Update Identification.java

--- Identification.java
+++ Identification.java
@@ -21,30 +21,36 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import projet_bulles.Jeu.STATE;
+
 public class Identification extends JFrame {
 
-	// private JPanel fenetre;
+//	 private JPanel fenetre;
+//
+//	JLabel myLabel;
 
-	JLabel myLabel;
 	private JButton bouton_seconnecter;
 	private JButton bouton_reset;
 	private static String a;
-	JPanel top;
+	//JPanel top;
 	private JTextField champ_saisie;
 	private static Utilisateur utilisateur;
 	private String[] utilisateurs;
+	
+	
+	static boolean connecte = true; // à changer
 
 	public Identification() throws IOException {
 		Utilisateur.recupIdentifiants();
 		this.utilisateurs = Utilisateur.getutilisateurs();
-		this.setContentPane(new ImagePanel(new ImageIcon(
-				"/home/ann2/daviaudl/Bureau/bulle1.jpg").getImage()));
+		this.setContentPane(new ImagePanel(new ImageIcon("bulle1.jpg").getImage()));
 		this.setTitle("La case a bulles");
 		this.setExtendedState(Frame.MAXIMIZED_BOTH);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		Font police = new Font("Verdana", Font.BOLD, 20);
 		this.setLayout(null);
+		//make userbox to enter users name
 		JComboBox<String> combo = MainPanel.makeComboBox(this.utilisateurs);
 		combo.setEditable(true);
 		combo.setSelectedIndex(-1);
@@ -56,14 +62,18 @@ public class Identification extends JFrame {
 		p.setBounds(750, 425, 200, 50);
 		p.add(combo, BorderLayout.CENTER);
 		this.getContentPane().add(p, BorderLayout.CENTER);
+		//////////////////////////////////////////
 		Insets insets = this.getInsets();
 		this.bouton_seconnecter = new JButton("Se connecter");
 		this.bouton_seconnecter.setBackground(Color.ORANGE);
 		this.bouton_seconnecter.setFont(police);
 		this.bouton_seconnecter.setBounds(1000, 400, 200, 75);
 		this.getContentPane().add(this.bouton_seconnecter, BorderLayout.CENTER);
+		
 		this.bouton_seconnecter.addActionListener(new ActionListener() {
+		
 			public void actionPerformed(ActionEvent e) {
+				Identification.connecte = true;
 				utilisateur = new Utilisateur(
 						champ_saisie.getText() == null ? "" : champ_saisie
 								.getText());
@@ -71,9 +81,25 @@ public class Identification extends JFrame {
 					utilisateur.Identification();
 				} catch (IOException e1) { // TODO Auto-generated catch block
 					e1.printStackTrace();
+					
 				}
 				System.out.println(utilisateur.toString());
+				if (connecte == true) {
+				Jeu.State = STATE.MENU;
+				  dispose(); // efface l'écran de connexion
+				try {
+					Jeu.controller();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				}
+				
 			}
+
+		
+			
+			
 		});
 		this.bouton_reset = new JButton("Reinitialiser les utilisateurs");
 		this.bouton_reset.setBackground(Color.WHITE);
@@ -81,8 +107,12 @@ public class Identification extends JFrame {
 		this.bouton_reset.setBounds(1480, 900, 400, 75);
 		this.getContentPane().add(this.bouton_reset, BorderLayout.SOUTH);
 		this.bouton_reset.addActionListener(new ActionListener() {
+			
 			public void actionPerformed(ActionEvent e) {
+				
+				
 				Utilisateur.reinitialisation(true);
+				
 			}
 		});
 		this.setVisible(true);
@@ -105,7 +135,9 @@ public class Identification extends JFrame {
 				System.out.println(utilisateur.toString());
 			}
 		});
+		
 		return a;
+		
 	}
 
 	/**
@@ -117,3 +149,4 @@ public class Identification extends JFrame {
 		// id1.execute();
 	}
 }
+
