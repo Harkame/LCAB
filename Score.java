3f1c3cbeb60935bc09dbbf55e95f655e331be00a
Harkame <louisr.daviaud@gmail.com>
2015-12-07 23:50:07 +0100

    Update Score.java

--- Score.java
+++ Score.java
@@ -3,62 +3,90 @@ package projet_bulles;
 import java.io.File;
 
 public class Score {
-	private int nombre_clic;
-	private int nombre_bulle;
+	private int nombre_clics;
+	private int nombre_bulles;
 	private static File fichier = new File("D:\\utilisateurs.txt");
+	private String[][] scores;
 
+	/*
+	 * Constructeur à vide, le score est de 0 clic - 0 bulle
+	 */
 	public Score() {
-		this.nombre_clic = 0;
-		this.nombre_bulle = 0;
+		this.nombre_clics = 0;
+		this.nombre_bulles = 0;
 	}
 
-	void recupScore(StringBuilder ligne) {
+	/*
+	 * Décompose le score de la ligne passé en paramètre enlève cette
+	 * information de cette dernière afin de "l'alléger", moins lourde pour les
+	 * prochains traitement dessus
+	 */
+	public void recupScore(StringBuilder ligne) {
 		ligne.delete(0, 1);
 		int i = 0;
-		StringBuilder nombre_clic = new StringBuilder();
-		StringBuilder nombre_bulle = new StringBuilder();
+		StringBuilder nombre_clics = new StringBuilder();
+		StringBuilder nombre_bulles = new StringBuilder();
 		while (ligne.charAt(i) != '-') {
-			nombre_clic.append(ligne.charAt(i));
+			nombre_clics.append(ligne.charAt(i));
 			i++;
 		}
 		i++;
 		while (ligne.charAt(i) != '|') {
-			nombre_bulle.append(ligne.charAt(i));
+			nombre_bulles.append(ligne.charAt(i));
 			i++;
 		}
-		this.nombre_clic = Integer.parseInt(nombre_clic.toString());
-		this.nombre_bulle = Integer.parseInt(nombre_bulle.toString());
+		this.nombre_clics = Integer.parseInt(nombre_clics.toString());
+		this.nombre_bulles = Integer.parseInt(nombre_bulles.toString());
 		ligne.delete(0, i);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString() toString() classic
+	 */
 	public String toString() {
 		StringBuilder score = new StringBuilder();
 		score.append("clics : ");
-		switch (String.valueOf(this.nombre_clic).length()) {
+		switch (String.valueOf(this.nombre_clics).length()) {
 		case 1:
-			score.append(this.nombre_clic + "  ");
+			score.append(this.nombre_clics + "  ");
 			break;
 		case 2:
-			score.append(this.nombre_clic + " ");
+			score.append(this.nombre_clics + " ");
 			break;
 		case 3:
-			score.append(this.nombre_clic);
+			score.append(this.nombre_clics);
 			break;
 		}
 		score.append(" - ");
-		switch (String.valueOf(this.nombre_bulle).length()) {
+		switch (String.valueOf(this.nombre_bulles).length()) {
 		case 1:
-			score.append("  " + this.nombre_bulle);
+			score.append("  " + this.nombre_bulles);
 			break;
 		case 2:
-			score.append(" " + this.nombre_bulle);
+			score.append(" " + this.nombre_bulles);
 			break;
 		case 3:
-			score.append(this.nombre_bulle);
+			score.append(this.nombre_bulles);
 			break;
 		}
 		score.append(" : bulles");
 		return score.toString();
+	}
+
+	/*
+	 * Getter nombre_clics d'un score (this)
+	 */
+	public int getnombre_clics() {
+		return this.nombre_clics;
+	}
+
+	/*
+	 * Getter nombre_bulles d'un score (this)
+	 */
+	public int getnombre_bulles() {
+		return this.nombre_bulles;
 	}
 
 	public static void main(String[] Args) {
