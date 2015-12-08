3b4408b04ce84cdf9b04445956128729846e929f
masskarad <masskaarad@gmail.com>
2015-12-08 10:55:01 +0100

    Update Annimation.java

--- Annimation.java
+++ Annimation.java
@@ -1,27 +1,18 @@
+ package projet_bulles;
 import javax.swing.JPanel;
 
+public class Annimation extends Thread {
+	private JPanel pan;
+	public Bulle bulle;
 
-public class Annimation extends Thread{
-		private JPanel pan;
-		private Bulle maBulle;
-		
-		
-		 public Annimation(String nom,JPanel pan) {
-		  super(nom);
-		 this.pan=pan;
-		 this.maBulle=new Bulle(500,500,0,0,this.pan);
-		 }
-	 
-		
-		 public void run() {
-			 
-			this.maBulle.animer();
-		   Thread.yield();
-		  
-		 }
-		 public Bulle getBulle(){
-			 return this.maBulle;
-		 }
-	  
-		
+	public Annimation(String nom, JPanel pan, int taille, int vitesse) {
+		super(nom);
+		this.pan=pan;
+		this.bulle=new Bulle(500,500,taille,vitesse,this.pan);
+	}
+
+	public void run() {
+		this.bulle.animer();
+	   	Thread.yield();	  
+	}	
 }
