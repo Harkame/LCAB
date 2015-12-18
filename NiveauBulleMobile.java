882e98b2fd20180e710e0b47b0e831d9f55ea624
Harkame <louisr.daviaud@gmail.com>
2015-12-18 16:12:53 +0100

    Delete NiveauBulleMobile.java

--- NiveauBulleMobile.java
+++
@@ -1,9 +0,0 @@
-
-public class NiveauBulleMobile extends NiveauBulle {
-	private long vitesseBulle;
-	
-	public NiveauBulleMobile(int nbreBulle,int tailleBulle, long vitesseBulle){
-		super(nbreBulle);
-		super.getPlateauBulle().afficherBulleMobile(tailleBulle,vitesseBulle);
-	}
-}
