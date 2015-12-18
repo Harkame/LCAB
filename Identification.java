d9bf15f716e1f5f368b8870ac6f63c3810499488
Harkame <louisr.daviaud@gmail.com>
2015-12-18 16:18:50 +0100

    Update Identification.java

--- Identification.java
+++ Identification.java
@@ -25,32 +25,32 @@ import projet_bulles.Jeu.STATE;
 
 public class Identification extends JFrame {
 
-//	 private JPanel fenetre;
-//
-//	JLabel myLabel;
+	// private JPanel fenetre;
+	//
+	// JLabel myLabel;
 
 	private JButton bouton_seconnecter;
 	private JButton bouton_reset;
 	private static String a;
-	//JPanel top;
+	// JPanel top;
 	private JTextField champ_saisie;
 	private static Utilisateur utilisateur;
 	private String[] utilisateurs;
-	
-	
+
 	static boolean connecte = true; // à changer
 
 	public Identification() throws IOException {
 		Utilisateur.recupIdentifiants();
 		this.utilisateurs = Utilisateur.getutilisateurs();
-		this.setContentPane(new ImagePanel(new ImageIcon("bulle1.jpg").getImage()));
+		this.setContentPane(new ImagePanel(new ImageIcon("bulle1.jpg")
+				.getImage()));
 		this.setTitle("La case a bulles");
 		this.setExtendedState(Frame.MAXIMIZED_BOTH);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		Font police = new Font("Verdana", Font.BOLD, 20);
 		this.setLayout(null);
-		//make userbox to enter users name
+		// make userbox to enter users name
 		JComboBox<String> combo = MainPanel.makeComboBox(this.utilisateurs);
 		combo.setEditable(true);
 		combo.setSelectedIndex(-1);
@@ -62,16 +62,16 @@ public class Identification extends JFrame {
 		p.setBounds(750, 425, 200, 50);
 		p.add(combo, BorderLayout.CENTER);
 		this.getContentPane().add(p, BorderLayout.CENTER);
-		//////////////////////////////////////////
+		// ////////////////////////////////////////
 		Insets insets = this.getInsets();
 		this.bouton_seconnecter = new JButton("Se connecter");
 		this.bouton_seconnecter.setBackground(Color.ORANGE);
 		this.bouton_seconnecter.setFont(police);
 		this.bouton_seconnecter.setBounds(1000, 400, 200, 75);
 		this.getContentPane().add(this.bouton_seconnecter, BorderLayout.CENTER);
-		
+
 		this.bouton_seconnecter.addActionListener(new ActionListener() {
-		
+
 			public void actionPerformed(ActionEvent e) {
 				Identification.connecte = true;
 				utilisateur = new Utilisateur(
@@ -86,21 +86,18 @@ public class Identification extends JFrame {
 				}
 				System.out.println(utilisateur.toString());
 				if (connecte == true) {
-				Jeu.State = STATE.MENU;
-				  dispose(); // efface l'écran de connexion
-				try {
-					Jeu.controller();
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
+					Jeu.State = STATE.MENU;
+					dispose(); // efface l'écran de connexion
+					try {
+						Jeu.controller();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
 				}
-				}
-				
+
 			}
 
-		
-			
-			
 		});
 		this.bouton_reset = new JButton("Reinitialiser les utilisateurs");
 		this.bouton_reset.setBackground(Color.WHITE);
@@ -108,10 +105,9 @@ public class Identification extends JFrame {
 		this.bouton_reset.setBounds(1480, 900, 400, 75);
 		this.getContentPane().add(this.bouton_reset, BorderLayout.SOUTH);
 		this.bouton_reset.addActionListener(new ActionListener() {
-			
+
 			public void actionPerformed(ActionEvent e) {
-				
-				
+
 				Utilisateur.reinitialisation(true);
 				dispose();
 				try {
@@ -120,15 +116,15 @@ public class Identification extends JFrame {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-				
+
 			}
 		});
 		this.setVisible(true);
 
 	}
 
-	public Utilisateur getutilisateur() {
-		return this.utilisateur;
+	public static Utilisateur getutilisateur() {
+		return utilisateur;
 	}
 
 	public String execute() {
@@ -143,13 +139,9 @@ public class Identification extends JFrame {
 				System.out.println(utilisateur.toString());
 			}
 		});
-		
+
 		return a;
-		
-	}
-	
-	public Utilisateur getutilisateur(){
-		return utilisateur;
+
 	}
 
 	/**
@@ -161,4 +153,3 @@ public class Identification extends JFrame {
 		// id1.execute();
 	}
 }
-
