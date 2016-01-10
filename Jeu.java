e3579f439e42928437f766310c5844b89f2aa4e4
PraetCom <cedric_duriez@hotmail.fr>
2016-01-10 15:12:51 +0100

    Update Jeu.java

--- Jeu.java
+++ Jeu.java
@@ -72,9 +72,9 @@ public class Jeu extends Canvas implements Runnable {
 			// !!!!!!! IMPORTANT, il faudra faire le bouton jouer pour revenir
 			// au niveau jouÃƒÆ’Ã‚Â© ÃƒÆ’Ã‚Â  la derniÃƒÆ’Ã‚Â¨re fois par le joueur
 			// NiveauBulle N1= new NiveauBulleStatic(9,4);
-			PlateauBulle N1 = new PlateauBulle(9, 4, 0); // niveau bulle static
+		//	PlateauBulle N1 = new PlateauBulle(9, 4, 0); // niveau bulle static
 		
-
+// State inutilisable puisque l'appel de getpallier ici fait des bugs d'affichage
 		} else if (State == STATE.SELECTION_NIVEAU) {
 			ChoisirNiveau ch1 = new ChoisirNiveau();
 		}
