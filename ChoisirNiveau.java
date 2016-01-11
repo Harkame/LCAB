aa0c979b3b8af3d66597d0af146b584e1a3f7108
ArkynStar <maximin.guicha@gmail.com>
2016-01-11 13:35:15 +0100

    Update ChoisirNiveau.java

--- ChoisirNiveau.java
+++ ChoisirNiveau.java
@@ -19,20 +19,20 @@ import projet_bulles.Jeu.STATE;
 
 public class ChoisirNiveau extends JFrame implements KeyListener {
 
-	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-	int width = (int) screenSize.getWidth();
+	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize(); // ces lignes vont servir a connaitre
+	int width = (int) screenSize.getWidth();//la taille des ecrans de l'utilisateur.
 	int height = (int) screenSize.getHeight();
-	private JButton NiveauStatique1;
-	private JButton NiveauStatique2;
-	private JButton NiveauStatique3;
-	private JButton NiveauStatique4;
-	private JButton NiveauMobile1;
-	private JButton NiveauMobile2;
-	private JButton NiveauMobile3;
-	private JButton NiveauMobile4;
-	private JButton MenuPrincipal;
-	double w = width / 1.75;
-	double h = height / 1.5;
+	private JButton NiveauStatique1;//chaque bouton correspondra a un bouton de palier
+	private JButton NiveauStatique2;//NiveauStatique2 est le 2eme palier du mode statique.
+	private JButton NiveauStatique3;//
+	private JButton NiveauStatique4;//
+	private JButton NiveauMobile1;// NiveauMobile est le 1er palier du mode mobile
+	private JButton NiveauMobile2;//
+	private JButton NiveauMobile3;//
+	private JButton NiveauMobile4;//
+	private JButton MenuPrincipal;//
+	double w = width / 1.75;  //on prend dans une variable la largeur de l'ecran divisee par 1.75
+	double h = height / 1.5;  // on prend dans une variable la hauteur de l'ecran divisee par 1.5
 
 	public ChoisirNiveau() {
 
@@ -41,18 +41,18 @@ public class ChoisirNiveau extends JFrame implements KeyListener {
 
 		/* Redimensionner les images pour travailler sur tous les Ã©crans */
 		// if(width == 1920 && height == 1080) {
-		this.setContentPane(new ImagePanel(new ImageIcon("wallpaper.jpg").getImage())); // Ã 
+		this.setContentPane(new ImagePanel(new ImageIcon("wallpaper.jpg").getImage())); // le fond de la fenetre
 																						// reprendre
 		// }
 		this.setVisible(true);
 		this.setLayout(null); // permet le position correcte des boutons
 
-		Font police = new Font("Verdana", Font.BOLD, 15);
+		Font police = new Font("Verdana", Font.BOLD, 15);  // on choisi la police du texte
 
-		this.NiveauStatique1 = new JButton("Niveau Statique1");
-		this.NiveauStatique1.setBackground(Color.WHITE);
-		this.NiveauStatique1.setFont(police);
-		this.NiveauStatique1.setBounds(width / 10, height / 6, width / 5, height / 10);
+		this.NiveauStatique1 = new JButton("Niveau Statique1"); // on cree le bouton qui permettra de lancer un palier
+		this.NiveauStatique1.setBackground(Color.WHITE);// statique de difficulte 1, et on le place au bon endroit
+		this.NiveauStatique1.setFont(police);// sur la fenetre
+		this.NiveauStatique1.setBounds(width / 10, height / 6, width / 5, height / 10);  
 		this.getContentPane().add(this.NiveauStatique1);
 
 		this.NiveauStatique2 = new JButton("Niveau Statique2");
@@ -73,8 +73,8 @@ public class ChoisirNiveau extends JFrame implements KeyListener {
 		this.NiveauStatique4.setBounds(width / 10, (int) h, width / 5, height / 10);
 		this.getContentPane().add(this.NiveauStatique4);
 
-		this.NiveauMobile1 = new JButton("Niveau mobile1");
-		this.NiveauMobile1.setBackground(Color.WHITE);
+		this.NiveauMobile1 = new JButton("Niveau mobile1"); // on cree le bouton qui permettra de lancer le palier 
+		this.NiveauMobile1.setBackground(Color.WHITE); // de niveau mobile de difficulte 1 et on le place 
 		this.NiveauMobile1.setFont(police);
 		this.NiveauMobile1.setBounds((int) w, height / 6, width / 5, height / 10);
 		this.getContentPane().add(this.NiveauMobile1);
@@ -97,20 +97,20 @@ public class ChoisirNiveau extends JFrame implements KeyListener {
 		this.NiveauMobile4.setBounds((int) w, (int) h, width / 5, height / 10);
 		this.getContentPane().add(this.NiveauMobile4);
 
-		this.MenuPrincipal = new JButton("Menu Principal");
-		this.MenuPrincipal.setBackground(Color.WHITE);
+		this.MenuPrincipal = new JButton("Menu Principal"); // on place un bouton permettant 
+		this.MenuPrincipal.setBackground(Color.WHITE);// de revenir au menu principal
 		this.MenuPrincipal.setFont(police);
 		this.MenuPrincipal.setBounds((int) (width / 3 * 1.05), height / 2, width / 6, height / 10);
 		this.getContentPane().add(this.MenuPrincipal, BorderLayout.CENTER);
 
-		this.NiveauStatique1.addActionListener(new ActionListener() {
+		this.NiveauStatique1.addActionListener(new ActionListener() {  // on choisi l'action au clic du bouton
 
 			public void actionPerformed(ActionEvent e) {
 
 				dispose();
 
-				PlateauBulle N1 = new PlateauBulle(5, 1, 0);
-
+				PlateauBulle N1 = new PlateauBulle(5, 1, 0); // on lance un palier correspond a la difficulte 1 
+									     // d'un niveau statique
 			}
 		});
 
@@ -193,7 +193,7 @@ public class ChoisirNiveau extends JFrame implements KeyListener {
 
 		this.setVisible(true);
 
-		this.MenuPrincipal.addActionListener(new ActionListener() {
+		this.MenuPrincipal.addActionListener(new ActionListener() { // on choisi l'action au lic du bouton menu principal
 
 			public void actionPerformed(ActionEvent e) {
 				Jeu.State = STATE.MENU;
