ec4a47222c9e9b0b263c482481b0fe10405021bb
Harkame <louisr.daviaud@gmail.com>
2016-01-16 02:50:36 +0100

    Update Identification.java

--- Identification.java
+++ Identification.java
@@ -38,27 +38,29 @@ public class Identification extends JFrame {
 	private static Utilisateur utilisateur;
 	private String[] utilisateurs;
 
-	static boolean connecte = true; // ÃƒÆ’Ã‚Â  changer
+	static boolean connecte = true; // ÃƒÆ’Ã†â€™Ãƒâ€šÃ‚Â  changer
 
 	public Identification() throws IOException {
 		this.setContentPane(new ImagePanel(new ImageIcon(getClass()
-				.getResource("wallpaper.jpg")).getImage())); // on met le fond
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize(); // RÃƒÂ©cupÃƒÂ©ration
+				.getResource("/wallpaper.jpg")).getImage())); // on met le fond
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize(); // RÃƒÆ’Ã‚Â©cupÃƒÆ’Ã‚Â©ration
 																			// des
 																			// dimensions
 																			// de
-																			// l'ÃƒÂ©cran
+																			// l'ÃƒÆ’Ã‚Â©cran
 		int width = (int) screenSize.getWidth(); // La largeur
 		int height = (int) screenSize.getHeight(); // La hauteur
-		final double w = width / 1.5; // La moitiÃƒÂ© de l'ÃƒÂ©cran
-		Utilisateur.recupIdentifiants(); // On rÃƒÂ©cup_re tous les
+		final double w = width / 1.5; // La moitiÃƒÆ’Ã‚Â© de l'ÃƒÆ’Ã‚Â©cran
+		Utilisateur.recupIdentifiants(); // On rÃƒÆ’Ã‚Â©cup_re tous les
 											// identifiants, utile pour la
 											// suggestion
-		this.utilisateurs = Utilisateur.getutilisateurs(); // On prÃƒÂ©pare la
+		this.utilisateurs = Utilisateur.getutilisateurs(); // On prÃƒÆ’Ã‚Â©pare
+															// la
 															// suggestions
-		this.setTitle("La case a bulles"); // Nommage de la fenÃƒÂªtre (titre)
-		this.setExtendedState(Frame.MAXIMIZED_BOTH); // FenÃƒÂªtre en plein
-														// ÃƒÂ©cran
+		this.setTitle("La case a bulles"); // Nommage de la fenÃƒÆ’Ã‚Âªtre
+											// (titre)
+		this.setExtendedState(Frame.MAXIMIZED_BOTH); // FenÃƒÆ’Ã‚Âªtre en plein
+														// ÃƒÆ’Ã‚Â©cran
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Si on clic sur
 																// la croix
 																// rouge (en
@@ -66,26 +68,28 @@ public class Identification extends JFrame {
 																// droite), sa
 																// fermera le
 																// processus
-		this.setLocationRelativeTo(null); // On localisation de la fenÃƒÂªtre
-											// est indÃƒÂ©pendante
+		this.setLocationRelativeTo(null); // On localisation de la
+											// fenÃƒÆ’Ã‚Âªtre
+											// est indÃƒÆ’Ã‚Â©pendante
 		Font police = new Font("Verdana", Font.BOLD, 20); // Police lisible et
 															// suffisament
 															// grosse
-		this.setLayout(null); // La fenÃƒÂªtre est null au dÃƒÂ©part (utile pour
+		this.setLayout(null); // La fenÃƒÆ’Ã‚Âªtre est null au dÃƒÆ’Ã‚Â©part
+								// (utile pour
 								// la modifier comme on veux ensuite)
 		// make userbox to enter users name
 		JComboBox<String> combo = MainPanel.makeComboBox(this.utilisateurs); // On
-																				// crÃƒÂ©e
+																				// crÃƒÆ’Ã‚Â©e
 																				// les
 																				// suggestions
 		combo.setEditable(true); // La combo est modifiable
 		combo.setSelectedIndex(-1); // A la base il n'y a rien dedans
-		this.champ_saisie = (JTextField) combo.getEditor().getEditorComponent(); // CrÃƒÂ©ation
+		this.champ_saisie = (JTextField) combo.getEditor().getEditorComponent(); // CrÃƒÆ’Ã‚Â©ation
 																					// du
 																					// champ
 																					// de
 																					// saisie
-		this.champ_saisie.setText(""); // Par dÃƒÂ©fault il est vide
+		this.champ_saisie.setText(""); // Par dÃƒÆ’Ã‚Â©fault il est vide
 		this.champ_saisie.addKeyListener(new ComboKeyHandler(combo)); // On y
 																		// met
 																		// la
@@ -95,9 +99,10 @@ public class Identification extends JFrame {
 																		// suggestions
 																		// en
 																		// temps
-																		// rÃƒÂ©el
-		champ_saisie.setFont(police); // On lui donne la plice crÃƒÂ©e
-		JPanel p = new JPanel(new BorderLayout()); // on crÃƒÂ©e un panel pour
+																		// rÃƒÆ’Ã‚Â©el
+		champ_saisie.setFont(police); // On lui donne la plice crÃƒÆ’Ã‚Â©e
+		JPanel p = new JPanel(new BorderLayout()); // on crÃƒÆ’Ã‚Â©e un panel
+													// pour
 													// le champ de saisie
 		p.setBounds(width / 2 - width / 8, height / 3, width / 10, height / 20); // Definition
 																					// de
@@ -111,24 +116,26 @@ public class Identification extends JFrame {
 																					// position
 		p.add(combo, BorderLayout.CENTER); // On y ajoute le combo
 		this.getContentPane().add(p, BorderLayout.CENTER); // On l'ajoute a la
-															// fenÃƒÂªtre
+															// fenÃƒÆ’Ã‚Âªtre
 															// principale
 		// ////////////////////////////////////////
 		Insets insets = this.getInsets(); // ?
-		this.bouton_seconnecter = new JButton("Se connecter"); // CrÃƒÂ©ation du
+		this.bouton_seconnecter = new JButton("Se connecter"); // CrÃƒÆ’Ã‚Â©ation
+																// du
 																// bouton se
 																// connecter
 		this.bouton_seconnecter.setBackground(Color.ORANGE); // Coloration orane
 		this.bouton_seconnecter.setFont(police); // On lui donne la police
 		this.bouton_seconnecter.setBounds(width / 2, height / 3, width / 5,
-				height / 20); // DÃƒÂ©finition de sa taille et de la position
+				height / 20); // DÃƒÆ’Ã‚Â©finition de sa taille et de la
+								// position
 		this.getContentPane().add(this.bouton_seconnecter, BorderLayout.CENTER); // On
 																					// ajoute
 																					// le
 																					// bouton
-																					// ÃƒÂ 
+																					// ÃƒÆ’Ã‚Â 
 																					// la
-																					// fenÃƒÂªtre
+																					// fenÃƒÆ’Ã‚Âªtre
 		this.bouton_seconnecter.addActionListener(new ActionListener() { // On
 																			// donne
 																			// un
@@ -137,11 +144,12 @@ public class Identification extends JFrame {
 																			// bouton
 					public void actionPerformed(ActionEvent e) { // Si on clic
 																	// dessus
-						Identification.connecte = true; // On passe en
-														// connectÃƒÂ©
+						// Identification.connecte = true; // On passe en
+						// connectÃƒÆ’Ã‚Â©
 						utilisateur = new Utilisateur(
 								champ_saisie.getText() == null ? ""
-										: champ_saisie.getText()); // On crÃƒÂ©e
+										: champ_saisie.getText()); // On
+																	// crÃƒÆ’Ã‚Â©e
 																	// un nouvel
 																	// utilisateur
 																	// avec le
@@ -154,9 +162,10 @@ public class Identification extends JFrame {
 															// l'indentification
 															// de l'utilisateur
 															// (voir fonction)
-							dispose(); // On efface l'ÃƒÂ©cran
-							// new Identification(); //On en recrÃƒÂ©e une
-							// nouvelle (Sa met ÃƒÂ  jour les suggestions avec
+							dispose(); // On efface l'ÃƒÆ’Ã‚Â©cran
+							// new Identification(); //On en recrÃƒÆ’Ã‚Â©e une
+							// nouvelle (Sa met ÃƒÆ’Ã‚Â  jour les suggestions
+							// avec
 							// le nouvel utilisateur)
 						} catch (IOException e1) {
 						}
@@ -167,21 +176,30 @@ public class Identification extends JFrame {
 																	// l'utilisateur
 																	// courant,
 																	// permet de
-																	// verifiÃƒÂ©
+																	// verifiÃƒÆ’Ã‚Â©
 																	// rapidement
 																	// si sa a
-																	// marchÃƒÂ©
+																	// marchÃƒÆ’Ã‚Â©
 																	// ou pas
 																	// (utile
 																	// pour le
 																	// programmeur)
-						if (connecte == true) { // Si on est passer connecter
+						boolean valide = Utilisateur.getvalide();
+						if (valide == true) { // Si on est passer connecter
 							Jeu.State = STATE.MENU; // on est rediriger sur le
 													// menu
-							dispose(); // efface l'ÃƒÆ’Ã‚Â©cran de connexion
+							dispose(); // efface l'ÃƒÆ’Ã†â€™Ãƒâ€šÃ‚Â©cran de
+										// connexion
 							try {
 								Jeu.controller();
 							} catch (IOException e1) {
+							}
+						} else {
+							try {
+								new Identification();
+							} catch (IOException e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
 							}
 						}
 
@@ -195,15 +213,16 @@ public class Identification extends JFrame {
 		this.bouton_reset.setBackground(Color.WHITE); // Un fond blanc
 		this.bouton_reset.setFont(police); // On y met la police
 		this.bouton_reset.setBounds((int) w, height - height / 6,
-				(int) (width / 3.5), height / 15); // DÃƒÂ©finition de la taille
+				(int) (width / 3.5), height / 15); // DÃƒÆ’Ã‚Â©finition de la
+													// taille
 													// et de la position
 		this.getContentPane().add(this.bouton_reset, BorderLayout.SOUTH); // On
 																			// l'ajoute
 																			// a
 																			// la
-																			// fenÃƒÂªtre
+																			// fenÃƒÆ’Ã‚Âªtre
 																			// actuel
-		this.bouton_reset.addActionListener(new ActionListener() { // DÃƒÂ©fition
+		this.bouton_reset.addActionListener(new ActionListener() { // DÃƒÆ’Ã‚Â©fition
 																	// d'une
 																	// action au
 																	// moment du
@@ -211,17 +230,18 @@ public class Identification extends JFrame {
 					public void actionPerformed(ActionEvent e) { // Si on clic
 																	// dessus
 						Utilisateur.reinitialisation(true); // On
-															// rÃƒÂ©initialiser
+															// rÃƒÆ’Ã‚Â©initialiser
 															// le fichier texte,
 															// true indique
 															// qu'on demandera
 															// une confirmation
-															// ÃƒÂ 
+															// ÃƒÆ’Ã‚Â 
 															// l'utilisateur
-						dispose(); // On ÃƒÂ©fface l'ÃƒÂ©cran d'identification
+						dispose(); // On ÃƒÆ’Ã‚Â©fface l'ÃƒÆ’Ã‚Â©cran
+									// d'identification
 						try {
-							new Identification(); // On en recrÃƒÂ©e une
-													// nouvelle (Sa met ÃƒÂ 
+							new Identification(); // On en recrÃƒÆ’Ã‚Â©e une
+													// nouvelle (Sa met ÃƒÆ’Ã‚Â 
 													// jour les suggestions avec
 													// le nouvel utilisateur)
 						} catch (IOException e1) {
@@ -229,7 +249,7 @@ public class Identification extends JFrame {
 
 					}
 				});
-		this.setVisible(true); // On affiche la fenÃƒÂªtre d'identification
+		this.setVisible(true); // On affiche la fenÃƒÆ’Ã‚Âªtre d'identification
 	}
 
 	/*
